4de5e073683581fe9ef7162a2e2138ec756588e2
bara495 <barra.495@gmail.com>
2020-01-22 16:54:09 +0100

    commiting the code from before

---
+++ utils.py
@@ -0,0 +1,25 @@
+def load_data(nr_of_elements=1.0):
+    """
+    returns a specified amount of data
+    """
+    from keras.datasets import mnist
+    from sklearn.model_selection import train_test_split
+
+    seed_number = None # TODO: verify that it works 
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train, _, y_train, _ = train_test_split(x_train, y_train, train_size = nr_of_elements, random_state = seed_number)
+
+    return (x_train, y_train), (x_test, y_test)
+
+def label_data(nr_of_labels):
+    """ 
+    Function that simulates labeling of data and returns 
+    a specified amount of labeled data
+    """
+    labels = None
+
+    return labels
+
+    
